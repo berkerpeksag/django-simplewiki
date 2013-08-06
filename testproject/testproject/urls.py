cd5d38446dab2a3c447d73548d5d9a1fa34c93b2
Berker Peksag <berker.peksag@gmail.com>
2013-08-06 18:39:08 +0300

    Add login/ and /logout/ views.

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -5,5 +5,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
+
+    (r'^accounts/login/$', 'django.contrib.auth.views.login',
+     {'template_name': 'registration/login.html',
+      'extra_context': {'next': '/wiki'}}),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+     {'next_page': '/wiki'}),
+
     url(r'^wiki/', include('simplewiki.urls')),
 )
