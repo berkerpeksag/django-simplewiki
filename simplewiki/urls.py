562f8fff840e01fde7d49da9d68718ebb4769019
Berker Peksag <berker.peksag@gmail.com>
2013-08-09 18:43:08 +0300

    Add IndexView.

--- simplewiki/urls.py
+++ simplewiki/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
 
-from .views import DocumentDetailView
+from .views import DocumentDetailView, IndexView
 
 urlpatterns = patterns('simplewiki.views',
-    url(r'^$', TemplateView.as_view(template_name="simplewiki/index.html"),
-        name="index"),
-    url(r'^(?P<slug>[a-z0-9-]+)$', DocumentDetailView.as_view()),
+    url(r'^$', IndexView.as_view()),
+    url(r'^(?P<slug>[a-z0-9-]+)$', DocumentDetailView.as_view(), name='document_detail'),
 )
--- simplewiki/views.py
+++ simplewiki/views.py
@@ -3,6 +3,15 @@ from django.views.generic import DetailView, ListView
 from .models import Document
 
 
+class IndexView(ListView):
+
+    context_object_name = 'docs'
+    template_name = 'simplewiki/index.html'
+
+    def get_queryset(self):
+        return Document.objects.filter(is_published=True).order_by('-created_on')
+
+
 class DocumentDetailView(DetailView):
 
     model = Document
