27fe1361ac9b6133181da5b87d4b060e51051c1d
Berker Peksag <berker.peksag@gmail.com>
2013-08-12 05:14:28 +0300

    Add more clear CSS based default theme.

--- simplewiki/urls.py
+++ simplewiki/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .views import DocumentDetail, DocumentUpdate, DocumentIndex
 
 urlpatterns = patterns('simplewiki.views',
-    url(r'^$', DocumentIndex.as_view()),
+    url(r'^$', DocumentIndex.as_view(), name='document_index'),
     url(r'^(?P<slug>[a-z0-9-]+)$', DocumentDetail.as_view(), name='document_detail'),
     url(r'^(?P<slug>[a-z0-9-]+)/edit/$', DocumentUpdate.as_view(), name='document_update'),
 )
