1ec5384919cb28ac4e223f46a6cdd35fb10a5ff8
Berker Peksag <berker.peksag@gmail.com>
2013-09-16 06:20:55 +0300

    Do not permanent=True when debugging.
    Otherwise, browsers uses the cache which is a nightmare.

--- simplewiki/urls.py
+++ simplewiki/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, url
 from django.views.generic.base import RedirectView
 
@@ -5,7 +6,7 @@ from .settings import MAIN_PAGE_SLUG
 from .views import DocumentDetail, DocumentRevision
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url=MAIN_PAGE_SLUG, permanent=True), name='index'),
+    url(r'^$', RedirectView.as_view(url=MAIN_PAGE_SLUG, permanent=not settings.DEBUG), name='index'),
     url(r'^(?P<slug>[a-z0-9-]+)$', DocumentDetail.as_view(), name='detail'),
     url(r'^(?P<slug>[a-z0-9-]+)/edit/$', DocumentRevision.as_view(), name='update'),
 )
