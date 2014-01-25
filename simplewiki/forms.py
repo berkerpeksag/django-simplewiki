4b543ddeb9117f0fe7d98d5de2894bddabef511d
Berker Peksag <berker.peksag@gmail.com>
2014-01-25 18:07:27 +0200

    Customize content field.

--- simplewiki/forms.py
+++ simplewiki/forms.py
@@ -18,6 +18,9 @@ class RevisionForm(forms.ModelForm):
     class Meta:
         model = Revision
         fields = ('content', 'summary')
+        widgets = {
+            'content': forms.Textarea(attrs={'cols': 70, 'rows': 20}),
+        }
 
 RevisionFormSet = inlineformset_factory(Document, Revision, form=RevisionForm,
                                         extra=1, can_delete=False)
