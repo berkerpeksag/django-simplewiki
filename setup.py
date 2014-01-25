72492f76ac6c3998e549537d6b21d5d5d3d28efc
Berker Peksag <berker.peksag@gmail.com>
2014-01-25 18:08:35 +0200

    Cleanup setup.py.

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
+with open('requirements.txt') as fobj:
+    install_requires = fobj.read().splitlines()
 
 setup(
     name='django-simplewiki',
-    version='0.1-dev',
+    version='0.1',
     description='A simple Wiki application for Django.',
-    long_description='',
     author='Berker Peksag',
     author_email='berker.peksag@gmail.com',
     url='https://github.com/berkerpeksag/django-simplewiki',
-    packages=['simplewiki'],
-    platforms='any',
+    packages=find_packages(),
+    install_requires=install_requires,
     license='Mozilla Public License, v. 2.0',
     keywords='django wiki',
-    classifiers=(
+    classifiers=[
         'Framework :: Django',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-    ),
+    ],
 )
