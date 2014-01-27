7b50b063464b98ee0a476a80353f1e4d785f2f49
Berker Peksag <berker.peksag@gmail.com>
2014-01-27 05:14:20 +0200

    Remove simplewiki/settings.py.

--- simplewiki/settings.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function, unicode_literals
-
-from django.conf import settings
-
-WIKI_TEXT_FORMAT = getattr(settings, 'WIKI_TEXT_FORMAT', 'reST')
-MAIN_PAGE_SLUG = getattr(settings, 'MAIN_PAGE_SLUG', 'main-page')
