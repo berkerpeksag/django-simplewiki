a2ee4e8a72cbcb16112ce32026b6f655375ad63b
Berker Peksag <berker.peksag@gmail.com>
2015-10-30 19:22:43 +0200

    Add admin integration for revisions.

--- simplewiki/admin.py
+++ simplewiki/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
-from .models import Document
+from .models import Document, Revision
+
+
+class RevisionInline(admin.StackedInline):
+    model = Revision
+    readonly_fields = ['creator', 'content']
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     readonly_fields = ['created_on', 'updated_on']
+    inlines = [RevisionInline]
